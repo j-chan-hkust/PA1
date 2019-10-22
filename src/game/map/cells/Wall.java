123e7cf10ac413538f7931c884cc6e593338eea8
Jung Chan <jungchan2013@gmail.com>
2019-10-22 13:10:15 +0800

    Implementation of Wall done

--- src/game/map/cells/Wall.java
+++ src/game/map/cells/Wall.java
@@ -1,5 +1,6 @@
 package game.map.cells;
 
+import game.pipes.Pipe;
 import org.jetbrains.annotations.NotNull;
 import util.Coordinate;
 import util.PipePatterns;
@@ -13,7 +14,8 @@ public class Wall extends Cell {
      * @param coord coordination of {@code this} cell
      */
     public Wall(@NotNull Coordinate coord) {
-        // TODO
+        // TODO DONE
+        super(coord);
     }
 
     /**
@@ -25,7 +27,7 @@ public class Wall extends Cell {
      */
     @Override
     public char toSingleChar() {
-        // TODO
-        return '\0';
+        // TODO DONE
+        return PipePatterns.WALL;
     }
 }
