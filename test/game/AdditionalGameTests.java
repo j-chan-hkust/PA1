4ca6ec3889592ae6a250b3b0ce26605cff57a0a8
Jung Chan <jungchan2013@gmail.com>
2019-10-23 22:02:24 +0800

    renamed game test

--- test/game/AdditionalGameTests.java
+++ test/game/AdditionalGameTests.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class GameTests {
+class AdditionalGameTests {
     @Test
     void testHasLost() {
         final var cellRep =
